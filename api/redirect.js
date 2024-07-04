903bf7599bc49f937288fe2aa2b150760bca734f
hritikcore <hritiksethi@Hritiks-MacBook-Pro.local>
2024-07-04 20:47:12 +0530

    redirects fixed

--- api/redirect.js
+++ api/redirect.js
@@ -10,38 +10,14 @@ const proxyRequest = (url, req, res) => {
   req.pipe(proxy, { end: true });
 };
 
+// simple device related redirects
 export default function handler(req, res) {
   const userAgent = req.headers["user-agent"].toLowerCase();
   const isMobile = /android|iphone/.test(userAgent);
 
-  if (req.method === "POST" && req.url === "/submit") {
-    // Example of form submission handling logic
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", () => {
-      // Here you can handle the form data, e.g., save it or process it
-      console.log("Received form data:", body);
-
-      // After handling the form submission, redirect to the appropriate home.html
-      if (isMobile) {
-        res.writeHead(302, {
-          Location: "https://regalium-mobile.vercel.app/home.html",
-        });
-      } else {
-        res.writeHead(302, {
-          Location: "https://regalium-desktop.vercel.app/home.html",
-        });
-      }
-      res.end();
-    });
+  if (isMobile) {
+    proxyRequest("https://regalium-mobile.vercel.app" + req.url, req, res);
   } else {
-    // Proxy the request to the appropriate site
-    if (isMobile) {
-      proxyRequest("https://regalium-mobile.vercel.app" + req.url, req, res);
-    } else {
-      proxyRequest("https://regalium-desktop.vercel.app" + req.url, req, res);
-    }
+    proxyRequest("https://regalium-desktop.vercel.app" + req.url, req, res);
   }
 }
